1df704830a4b2e4beaec5983bbe68540f6c956b5
Rukiya takaesu <s23014@std.it-college.ac.jp>
2023-08-08 05:50:07 +0000

    mezotto

---
+++ CHAPTER05/Q5_1_3.py
@@ -0,0 +1,9 @@
+from collectinos import deque
+
+queue = deque([1, 2])
+queue.append(3)
+queue.append(4)
+print("queue:", queue)
+print("popleft from queue 1st value:", queue.popleft())
+print("popleft form queue 2nd value:", queue.popleft())
+print("queue:", queue)
