c59863b618345e71894ce9d0bb8947d3713ed0fe
Rukiya takaesu <s23014@std.it-college.ac.jp>
2023-08-10 06:13:27 +0000

    syusei

--- CHAPTER04/Q4_9_1.py
+++ CHAPTER04/Q4_9_1.py
@@ -1,19 +1,11 @@
-def bubble_sort_descending(data):
-    n = len(data)
-    for i in range(n - 1):
-        
-        for j in range(n - i - 1):
-            if data[j] < data[j + 1]:
-                data[j], data[j + 1] = data[j + 1], data[j]
+import eggs
+import ham
+import spam
+import x
+import y
 
-    return data
-
-if __name__ == "__main__":
-    N = int(input())
-    data = []
-    for _ in range(N):
-        d = int(input())
-        data.append(d)
-
-    sorted_data = bubble_sort_descending(data)
-    print(*sorted_data)
+spam(ham[1], {eggs: 2})
+foo = (0,)
+if x == 4:
+    print(x, y)
+    x, y = y, x
---
+++ CHAPTER04/Q4_9_3.py
@@ -0,0 +1,19 @@
+def bubble_sort_descending(data):
+    n = len(data)
+    for i in range(n - 1):
+        for j in range(n - i - 1):
+            if data[j] < data[j + 1]:
+                data[j], data[j + 1] = data[j + 1], data[j]
+
+    return data
+
+
+if __name__ == "__main__":
+    N = int(input())
+    data = []
+    for _ in range(N):
+        d = int(input())
+        data.append(d)
+
+    sorted_data = bubble_sort_descending(data)
+    print(*sorted_data)
