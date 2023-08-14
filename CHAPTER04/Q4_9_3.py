a10adfc8e8de3968614479f8878843e72f36f0d5
Rukiya takaesu <s23014@std.it-college.ac.jp>
2023-08-14 05:23:20 +0000

    test

--- CHAPTER04/Q4_9_3.py
+++ CHAPTER04/Q4_9_3.py
@@ -1,19 +1,13 @@
-def bubble_sort_descending(data):
-    n = len(data)
-    for i in range(n - 1):
-        for j in range(n - i - 1):
-            if data[j] < data[j + 1]:
-                data[j], data[j + 1] = data[j + 1], data[j]
+N = int(input())
+a = []
 
-    return data
+for _ in range(N):
+    num = int(input())
+    a.append(num)
 
+for i in range(N):
+    for j in range(i + 1, N):
+        if a[i] < a[j]:
+            a[i], a[j] = a[j], a[i]
 
-if __name__ == "__main__":
-    N = int(input())
-    data = []
-    for _ in range(N):
-        d = int(input())
-        data.append(d)
-
-    sorted_data = bubble_sort_descending(data)
-    print(*sorted_data)
+print(" ".join(map(str, a)))
